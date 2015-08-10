342e498ba1ba48bf4626fd55fb97b8f4b18663dd
Michelle <watersprite819@gmail.com>
2015-08-10 15:57:19 +0800

    tagging feature not completed viewing tag in one page doesn't work

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagController < ApplicationController
+
+  def index
+    @tag = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+      @tag = Tag.find(params[:id])
+      @tag.destroy
+      flash[:notice] = "Tag '#{@tag.name}' destroyed!"
+      redirect_to tags_path
+    end  
+end
